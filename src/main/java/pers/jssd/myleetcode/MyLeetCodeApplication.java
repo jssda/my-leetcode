c8bfebf0b809100cfb28b4c8af336644ddc006f9
jssd <jssdjing@gmail.com>
2022-07-19 23:48:31 +0800

    feat(init): init

---
+++ src/main/java/pers/jssd/myleetcode/MyLeetCodeApplication.java
@@ -0,0 +1,13 @@
+package pers.jssd.myleetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLeetCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLeetCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pers/jssd/myleetcode/MyLeetCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.jssd.myleetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLeetCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
