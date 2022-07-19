dd93d20f13a931b1b799dc73c982aa4c96ffd073
jssd <jssdjing@gmail.com>
2022-07-19 23:50:47 +0800

    feat(init): rename application

--- src/main/java/pers/jssd/myleetcode/MyLeetcodeApplication.java
+++ src/main/java/pers/jssd/myleetcode/MyLeetcodeApplication.java
@@ -3,11 +3,14 @@ package pers.jssd.myleetcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author root
+ */
 @SpringBootApplication
-public class MyLeetCodeApplication {
+public class MyLeetcodeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyLeetCodeApplication.class, args);
+        SpringApplication.run(MyLeetcodeApplication.class, args);
     }
 
 }
